091b60df5a0bffc8012ff81184900ef7678edefb
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2025-03-24 21:40:40 +0300

    HTTP Patch

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/theplayernumberzero/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/theplayernumberzero/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,5 +1,7 @@
 package com.theplayernumberzero.springboot.cruddemo.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.theplayernumberzero.springboot.cruddemo.dao.EmployeeDAO;
 import com.theplayernumberzero.springboot.cruddemo.entity.Employee;
 import com.theplayernumberzero.springboot.cruddemo.service.EmployeeService;
@@ -7,15 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
     private EmployeeService employeeService;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    public EmployeeRestController(EmployeeService employeeService){
+    public EmployeeRestController(EmployeeService employeeService, ObjectMapper objectMapper){
         this.employeeService = employeeService;
+        this.objectMapper = objectMapper;
     }
     //expose "/employees" and return list of employees
     @GetMapping("/employees")
@@ -50,4 +55,37 @@ public class EmployeeRestController {
         return employee;
     }
 
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload){
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if(tempEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        //user cant change id field
+        if(patchPayload.containsKey("id")){
+            throw new RuntimeException("You cant modify id field");
+        }
+
+        Employee patchedEmployee = apply(patchPayload, tempEmployee);
+
+        //save to the db
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+
+        return dbEmployee;
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
+        //employee obj to json
+        ObjectNode employeeNode = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+        //patchPayload map to json
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+
+        //merge patch and object
+        employeeNode.setAll(patchNode);
+
+        //convert back to object
+        return objectMapper.convertValue(employeeNode, Employee.class);
+    }
 }
