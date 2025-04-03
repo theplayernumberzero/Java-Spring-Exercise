da14f0e5ade04e85a2a3f27d3a0e5ca865dff6d2
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2025-04-03 09:09:47 +0300

    Spring Security - JDBC authentication with bcrypt and custom tables

--- 07-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/theplayernumberzero/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 07-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/theplayernumberzero/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,33 +3,26 @@ package com.theplayernumberzero.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    //add support for jdbc. No more hard coded user information
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-        UserDetails bahadir = User.builder()
-                .username("bahadir")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails umut = User.builder()
-                .username("umut")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails kilic = User.builder()
-                .username("kilic")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        return new InMemoryUserDetailsManager(bahadir, umut, kilic);
+        //how to find user
+        userDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id = ?");
+        //how to find role
+        userDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id = ?");
+
+        return userDetailsManager;
     }
 
     @Bean
@@ -54,3 +47,25 @@ public class DemoSecurityConfig {
         return http.build();
     }
 }
+
+/*
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails bahadir = User.builder()
+                .username("bahadir")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails umut = User.builder()
+                .username("umut")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+        UserDetails kilic = User.builder()
+                .username("kilic")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(bahadir, umut, kilic);
+    }*/
