8f4087d7b7f197c2ba5422c4459326adc7e1d5c3
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2025-04-04 14:55:35 +0300

    Jpa/Hibernate advanced mapping -bi directional

---
+++ 08-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/theplayernumberzero/cruddemo/dao/AppDaoImpl.java
@@ -0,0 +1,53 @@
+package com.theplayernumberzero.cruddemo.dao;
+
+import com.theplayernumberzero.cruddemo.entity.Instructor;
+import com.theplayernumberzero.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDaoImpl implements AppDao{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDaoImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int id) {
+        return entityManager.find(Instructor.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+
+        Instructor tempInstructor = findInstructorById(id);
+
+        entityManager.remove(tempInstructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int id) {
+        return entityManager.find(InstructorDetail.class, id);
+    }
+
+    @Override
+    public void deleteInstructorDetailById(int id) {
+        InstructorDetail tempInstructorDetail = findInstructorDetailById(id);
+
+        entityManager.remove(tempInstructorDetail);
+    }
+
+
+}
